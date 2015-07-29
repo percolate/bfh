21224c4e9fdc785c85141765148704b2ea7ab485
Evan Bender <evan.bender@percolate.com>
2015-07-28 21:32:20 -0400

    update url in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         "python-dateutil",
     ],
     author_email="evan.bender@percolate.com",
-    url="https://github.com/benauthor/bfh",
+    url="https://github.com/percolate/bfh",
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
